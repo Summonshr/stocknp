25a495a1bc953c78528e8b5191d4c97bb141b5f0
suman shrestha <summonshr@gmail.com>
2020-05-27 14:36:56 +0545

    WIP

--- routes/api.php
+++ routes/api.php
@@ -18,18 +18,27 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('home',function(){
-	return ['home'=>\Wink\WinkPost::with('author','tags')->latest()->get()->map(function($post){
-$post->featured_image = url($post->featured_image);
-$post->body = str_replace('"/storage/wink','"'.url('/storage/wink//'), $post->body);
-$post->author->avatar = url($post->author->avatar);
-return $post;})];
+Route::get('home', function () {
+    return ['home' => \Wink\WinkPost::with('author', 'tags')->latest()->get()->map(function ($post) {
+        $post->featured_image = url($post->featured_image);
+        $post->body = str_replace('"/storage/wink', '"' . url('/storage/wink//'), $post->body);
+        $post->author->avatar = url($post->author->avatar);
+        return $post;
+    })];
 });
 
-Route::get('tags/{slug?}', function($slug){
-        return ['news'=>\Wink\WinkPost::with('author','tags')->whereHas('tags', function($query) use($slug) {return $query->where('tag_id', \Wink\WinkTag::where('slug',$slug)->firstOrFail()->id);})->latest()->get()->map(function($post){
-$post->featured_image = url($post->featured_image);
-$post->body = str_replace('"/storage/wink','"'.url('/storage/wink//'), $post->body);
-$post->author->avatar = url($post->author->avatar);
-return $post;})];
+Route::get('tags/{slug?}', function ($slug) {
+    return ['news' => \Wink\WinkPost::with('author', 'tags')->whereHas('tags', function ($query) use ($slug) {
+        return $query->where('tag_id', \Wink\WinkTag::where('slug', $slug)->firstOrFail()->id);
+    })->latest()->get()->map(function ($post) {
+        $post->featured_image = url($post->featured_image);
+        $post->body = str_replace('"/storage/wink', '"' . url('/storage/wink//'), $post->body);
+        $post->author->avatar = url($post->author->avatar);
+        return $post;
+    })];
+});
+
+
+Route::get('companies', function () {
+    return \App\Company::all();
 });
