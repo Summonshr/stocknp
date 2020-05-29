b810de1852c793c4fa8e1dff8b2c492cbd918aa0
suman shrestha <summonshr@gmail.com>
2020-05-29 12:23:49 +0545

    Added models

--- app/Company.php
+++ app/Company.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+
+    public function news()
+    {
+        return $this->hasMany(News::class);
+    }
+
+    public function reports()
+    {
+        return $this->hasOne(Report::class);
+    }
+
+    public function prices()
+    {
+        return $this->hasMany(SharePrice::class);
+    }
+
+    public function dividends()
+    {
+        return $this->hasMany(Dividend::class);
+    }
 }
---
+++ resources/views/livewire/company.blade.php
@@ -0,0 +1,34 @@
+<div>
+    <table>
+        <thead>
+            <tr>
+                <td><input placeholder="name" wire:model="search"></td>
+                <td><select wire:model="sector">
+                        @foreach($sectors as $sector)
+                        <option value="{{$sector}}">
+                            {{$sector}}
+                        </option>
+                        @endforeach
+                    </select></td>
+                <td><select wire:model="type">
+                        @foreach($types as $type)
+                        <option value="{{$type}}">
+                            {{$type}}
+                        </option>
+                        @endforeach
+                    </select></td>
+                <td></td>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($companies as $company)
+            <tr>
+                <td title="{{$company->name}}">{{$company->symbol}}</td>
+                <td>{{$company->sector_name}}</td>
+                <td>{{$company->type}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    {{ $companies->links() }}
+</div>
