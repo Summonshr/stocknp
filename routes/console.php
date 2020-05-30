cd4096ee4bdd1f87e7383b001356b501de0d8f2f
suman shrestha <summonshr@gmail.com>
2020-05-30 14:00:09 +0545

    WIP

--- routes/console.php
+++ routes/console.php
@@ -1,1 +1,19 @@
 <?php
+
+use Illuminate\Support\Facades\Artisan;
+
+Artisan::call('fetch', function () {
+
+
+    \App\Company::where('eps', '=', 0)->each(function ($company) {
+        $data = json_decode(file_get_contents('https://bizmandu.com/__stock/tearsheet/financial/keyStats/?tkr=' . $company->symbol))->message->data[0] ?? false;
+        if ($data) {
+            $company->forceFill([
+                'eps' => $data->EpsAnnualized,
+                'bvps' => $data->BookValuePerShare,
+                'npl' => ($data->NonPerformingLoanNplToTotalLoan ?? 0) * 100
+            ]);
+            $company->save();
+        }
+    });
+});
