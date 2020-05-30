b9ddb0de24d5d9a1a79b5cca27239fb0a724d650
suman shrestha <summonshr@gmail.com>
2020-05-30 15:45:09 +0545

    WIP

--- routes/api.php
+++ routes/api.php
@@ -32,3 +32,23 @@
     Route::get('companies', function () {
         return \App\Company::all();
     });
+
+    Route::get('fetch', function () {
+        \App\Company::where('income', '0')->get()->each(function ($company) {
+            print($company->symbol);
+            $data = Http::timeout(2)->get('https://bizmandu.com/__stock/tearsheet/financial/keyStats/?tkr=' . $company->symbol)['message']['data'][0] ?? false;
+            if ($data && ($data['GrowthOverPriorPeriod'] ?? false)) {
+                $company->growth = $data['GrowthOverPriorPeriod'] * 100;
+                $company->eps = $data['EpsAnnualized'];
+                $company->bvps = $data['BookValuePerShare'];
+                $company->npl = ($data['NonPerformingLoanNplToTotalLoan'] ?? 0) * 100;
+                $company->income = $data['NetIncome'] * 100;
+            }
+
+            $data = json_decode(file_get_contents('https://bizmandu.com/__stock/tearsheet/header/?tkr=' . $company->symbol))->message->latestPrice ?? false;
+            if ($data)
+                $company->price = $data;
+
+            $company->save();
+        });
+    });
--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('fetch', function () {
     \App\Company::where('income', '0')->get()->each(function ($company) {
+        dd(Http::get("https://bizmandu.com/__stock/tearsheet/financial/keyStats/?tkr=STPO"));
         print($company->symbol);
         $data = json_decode(file_get_contents('https://bizmandu.com/__stock/tearsheet/financial/keyStats/?tkr=' . $company->symbol))->message->data[0] ?? false;
         if ($data && ($data->GrowthOverPriorPeriod ?? false)) {
