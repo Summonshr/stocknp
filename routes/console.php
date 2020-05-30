9205b7203a29e014795c4d9147fd5e4ea0bcb353
suman shrestha <summonshr@gmail.com>
2020-05-30 15:47:22 +0545

    WIP

--- routes/console.php
+++ routes/console.php
@@ -4,18 +4,17 @@ use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('fetch', function () {
     \App\Company::where('income', '0')->get()->each(function ($company) {
-        dd(Http::get("https://bizmandu.com/__stock/tearsheet/financial/keyStats/?tkr=STPO"));
         print($company->symbol);
-        $data = json_decode(file_get_contents('https://bizmandu.com/__stock/tearsheet/financial/keyStats/?tkr=' . $company->symbol))->message->data[0] ?? false;
-        if ($data && ($data->GrowthOverPriorPeriod ?? false)) {
-            $company->growth = $data->GrowthOverPriorPeriod * 100;
-            $company->eps = $data->EpsAnnualized;
-            $company->bvps = $data->BookValuePerShare;
-            $company->npl = ($data->NonPerformingLoanNplToTotalLoan ?? 0) * 100;
-            $company->income = $data->NetIncome * 100;
+        $data = Http::timeout(2)->get('https://bizmandu.com/__stock/tearsheet/financial/keyStats/?tkr=' . $company->symbol)['message']['data'][0] ?? false;
+        if ($data && ($data['GrowthOverPriorPeriod'] ?? false)) {
+            $company->growth = $data['GrowthOverPriorPeriod'] * 100;
+            $company->eps = $data['EpsAnnualized'];
+            $company->bvps = $data['BookValuePerShare'];
+            $company->npl = ($data['NonPerformingLoanNplToTotalLoan'] ?? 0) * 100;
+            $company->income = $data['NetIncome'] * 100;
         }
 
-        $data = json_decode(file_get_contents('https://bizmandu.com/__stock/tearsheet/header/?tkr=' . $company->symbol))->message->latestPrice ?? false;
+        $data = Http::timeout(2)->get('https://bizmandu.com/__stock/tearsheet/financial/keyStats/?tkr=' . $company->symbol)['message']['latestPrice'] ?? false;
         if ($data)
             $company->price = $data;
 
