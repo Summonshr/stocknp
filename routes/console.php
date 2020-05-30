89eaa03497fa423a1f4d715a27e17f4b3766927c
suman shrestha <summonshr@gmail.com>
2020-05-30 15:51:56 +0545

    WIP

--- routes/console.php
+++ routes/console.php
@@ -3,21 +3,25 @@
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('fetch', function () {
-    \App\Company::all()->each(function ($company) {
-        print($company->symbol);
-        $data = Http::timeout(2)->get('https://bizmandu.com/__stock/tearsheet/financial/keyStats/?tkr=' . $company->symbol)['message']['data'][0] ?? false;
-        if ($data && ($data['GrowthOverPriorPeriod'] ?? false)) {
-            $company->growth = $data['GrowthOverPriorPeriod'] * 100;
-            $company->eps = $data['EpsAnnualized'];
-            $company->bvps = $data['BookValuePerShare'];
-            $company->npl = ($data['NonPerformingLoanNplToTotalLoan'] ?? 0) * 100;
-            $company->income = $data['NetIncome'] * 100;
-        }
+    try {
+        \App\Company::all()->each(function ($company) {
+            print($company->symbol);
+            $data = Http::timeout(2)->get('https://bizmandu.com/__stock/tearsheet/financial/keyStats/?tkr=' . $company->symbol)['message']['data'][0] ?? false;
+            if ($data && ($data['GrowthOverPriorPeriod'] ?? false)) {
+                $company->growth = $data['GrowthOverPriorPeriod'] * 100;
+                $company->eps = $data['EpsAnnualized'];
+                $company->bvps = $data['BookValuePerShare'];
+                $company->npl = ($data['NonPerformingLoanNplToTotalLoan'] ?? 0) * 100;
+                $company->income = $data['NetIncome'] * 100;
+            }
 
-        $data = Http::timeout(2)->get('https://bizmandu.com/__stock/tearsheet/header/?tkr=' . $company->symbol)['message']['latestPrice'] ?? false;
-        if ($data)
-            $company->price = $data;
+            $data = Http::timeout(2)->get('https://bizmandu.com/__stock/tearsheet/header/?tkr=' . $company->symbol)['message']['latestPrice'] ?? false;
+            if ($data)
+                $company->price = $data;
 
-        $company->save();
-    });
+            $company->save();
+        });
+    } catch (\Throwable $th) {
+        //throw $th;
+    }
 });
