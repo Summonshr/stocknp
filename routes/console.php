87ed3d2c7831f480b12a23ddafb1fe37c5e9f07b
suman shrestha <summonshr@gmail.com>
2020-05-30 15:19:56 +0545

    WIP

--- routes/console.php
+++ routes/console.php
@@ -7,6 +7,9 @@ Artisan::command('fetch', function () {
         $data = json_decode(file_get_contents('https://bizmandu.com/__stock/tearsheet/financial/keyStats/?tkr=' . $company->symbol))->message->data[0] ?? false;
         if ($data && ($data->GrowthOverPriorPeriod ?? false)) {
             $company->growth = $data->GrowthOverPriorPeriod * 100;
+            $company->eps = $data->EpsAnnualized;
+            $company->bvps = $data->BookValuePerShare;
+            $company->npl = $data->NonPerformingLoanNplToTotalLoan * 100;
             $company->income = $data->NetIncome * 100;
             $company->save();
         }
